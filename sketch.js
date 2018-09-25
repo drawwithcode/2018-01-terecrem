87370c84b4759a0c0818718fd26038a487927a9b
terecrem <teresacremo@hotmail.it>
2018-09-25 18:10:05 +0200

    My Robot is moving on the X and it's chaging color

--- sketch.js
+++ sketch.js
@@ -7,17 +7,44 @@ function setup() {
 }
 
 function draw() {
-	background(204);
+	background(mouseX,220,220,95);
 
 	//Neck
-	line(250, 285, 250, 345);
+	line(mouseX, 285, mouseX, 345);
+strokeWeight(1)
+stroke(0,0,0)
+
 
 	//Wheel
-	ellipse(250, 465, 70, 70);
+		fill(mouseX,38,90)
+	ellipse(mouseX, 465, 70, 70,60);
 
-	//Body
-	rect(200, 325, 100, 140);
+	fill(mouseX,180,160)
+rect(mouseX-50, 325, 100, 140);
 
-	//Head
-	ellipse(250, 240, 90, 90);
+fill(mouseX,149,137)
+rect(mouseX-50, 325, 100, 20);
+
+		fill(mouseX,38,90)
+	ellipse(mouseX, 240, 90, 90);
+
+	fill(mouseX,38,90)
+ellipse(mouseX-60, 260, 10, 10);
+
+fill(mouseX,38,90)
+ellipse(mouseX-65, 240, 5, 5);
+
+fill(mouseX,38,90)
+ellipse(mouseX+55, 200, 12, 12);
+
+//occhi neri
+
+fill(0,0,0)
+ellipse(mouseX+25, 230, 3, 3);
+
+fill(0,0,0)
+ellipse(mouseX+18, 230, 3, 3);
+
+
+
 }
